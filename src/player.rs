a028dbf5572788303b7444c279486e959bce0127
Ben Brunton <benbrunton@hotmail.com>
2019-07-30 21:26:09 +0100

    consistent diagonal speed

--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,8 @@
+use std::f32;
+
 // frame on which animation occurs
 const FRAME_CYCLE: i32 = 10;
+const WALKING_VELOCITY: f32 = 0.5;
 
 #[derive(Copy, Clone)]
 pub enum HorizontalDirection {
@@ -23,11 +26,14 @@ pub struct Player {
     horizontal_direction: HorizontalDirection,
     vertical_direction: VerticalDirection,
     cycle: f32,
-    lifecycle: i32
+    lifecycle: i32,
+    diagonal_velocity: f32,
 }
 
 impl Player {
     pub fn new() -> Player {
+        let diagonal_velocity = (f32::consts::PI/4.0)
+            .sin() * WALKING_VELOCITY as f32;
         Player {
             pos_x: 0.0,
             pos_y: 0.0,
@@ -38,40 +44,47 @@ impl Player {
             horizontal_direction: HorizontalDirection::Left,
             vertical_direction: VerticalDirection::Down,
             cycle: 0.0,
-            lifecycle: 0
+            lifecycle: 0,
+            diagonal_velocity
         }
     }
 
-    // todo - diagonal should not be quicker
     pub fn update(&mut self) {
         let mut moving = false;
 
         if self.moving_left {
-            self.pos_x -= 0.5;
+            self.pos_x -= self.get_horizontal_velocity();
             self.horizontal_direction = HorizontalDirection::Left;
             moving = true;
         }
 
         if self.moving_right {
-            self.pos_x += 0.5;
+            self.pos_x += self.get_horizontal_velocity();
             self.horizontal_direction = HorizontalDirection::Right;
             moving = true;
         }
 
+        if (self.moving_left || self.moving_right)
+            && !self.moving_up {
+            self.vertical_direction = VerticalDirection::Down;
+        }
+
         if self.moving_up {
-            self.pos_y -= 0.5;
+            self.pos_y -= self.get_vertical_velocity();
             self.vertical_direction = VerticalDirection::Up;
             moving = true;
         }
 
         if self.moving_down {
-            self.pos_y += 0.5;
+            self.pos_y += self.get_vertical_velocity();
             self.vertical_direction = VerticalDirection::Down;
             moving = true;
         }
 
         if moving {
             self.update_cycle();
+        } else {
+            self.reset_cycle();
         }
     }
 
@@ -133,4 +146,25 @@ impl Player {
         }
 
     }
+
+    fn reset_cycle(&mut self) {
+        self.cycle = 0.0;
+    }
+
+    fn get_horizontal_velocity(&self) -> f32 {
+        if self.moving_up || self.moving_down {
+            self.diagonal_velocity    
+        } else {
+            WALKING_VELOCITY
+        }
+    }
+
+    fn get_vertical_velocity(&self) -> f32 {
+        if self.moving_left || self.moving_right {
+            self.diagonal_velocity    
+        } else {
+            WALKING_VELOCITY
+        }
+    }
+
 }
